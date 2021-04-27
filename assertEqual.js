9bef315ed8f9e0f94e417e5a0bf6385d08c3b9a0
Sura Jeon <surajeon@gmail.com>
2021-04-27 00:33:17 +0000

    assertEqual file added

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+let assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("Assertion Failed: " + actual + " !== " + expected);
+  }
+
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
