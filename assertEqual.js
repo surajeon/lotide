0c112caef7206a1d9c8cb7be68381cd7b09e0a0a
Sura Jeon <surajeon@gmail.com>
2021-04-28 16:12:55 +0000

    emoji fixed

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,9 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`🛑🛑🛑 Assertion Passed: ${actual} === ${expected}`)
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
   } else if (actual !== expected) {
-    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
-
 };
 
 assertEqual("Lighthouse Labs", "Bootcamp");
