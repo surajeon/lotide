7a693983c7673e69e6621d6aeb439ce86b1022d5
Sura Jeon <surajeon@gmail.com>
2021-04-30 04:20:57 +0000

    added findKey.js

---
+++ findKey.js
@@ -0,0 +1,33 @@
+
+const findKey = (object, callback) => {
+  for (const starName in object) {
+    if(callback(object[starName])) {
+      return starName;
+    }
+  }
+};
+  
+
+const moons = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+
+console.log(findKey(moons, x => x.stars === 3));
+
+
+
+
+
+
+
+
+
+
+
+
+
