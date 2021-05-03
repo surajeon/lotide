7b47f8e56e9adc97436c3eecf31fabbf0377ff6d
Sura Jeon <surajeon@gmail.com>
2021-05-03 20:57:30 +0000

    refactoring done: tail.js

--- tail.js
+++ tail.js
@@ -1,14 +1,3 @@
-let assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ✅✅✅ ${actual} === ${expected}`)
-  } else if (actual !== expected) {
-    
-    console.assert(false, `🛑🛑🛑 ${actual} !== ${expected}`);
-  }
-};
-
-
-
 const tail = function (elms) {
   let arr = [];
   for (let i = 1; i < elms.length; i++) {
@@ -17,7 +6,4 @@ const tail = function (elms) {
   return arr.join(", ");
 }
 
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,4 @@
+const tail = require("../tail");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words));
