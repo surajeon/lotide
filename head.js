1bed8bc0c0bd09aeddbcb542e795abef0924cc99
Sura Jeon <surajeon@gmail.com>
2021-05-03 20:19:02 +0000

    module test: added head.js

--- head.js
+++ head.js
@@ -1,19 +1,5 @@
-let assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ✅✅✅ ${actual} === ${expected}`)
-  } else if (actual !== expected) {
-    
-    console.assert(false, `🛑🛑🛑 ${actual} !== ${expected}`);
-  }
-};
-
-let head = function(elm) {
-  return elm[0];
+const head = function(elm) {
+  console.log(elm[0]);
 }
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
