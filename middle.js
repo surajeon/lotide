461215278fa2b8da58f4263a77da934dcc8ff993
Sura Jeon <surajeon@gmail.com>
2021-05-03 22:03:56 +0000

    refactoring done: middle.js

--- middle.js
+++ middle.js
@@ -1,32 +1,3 @@
-const eqArrays = function(actual, expected) {
-  // checking if length of the arrays are the same
-  if (actual.length !== expected.length) {
-      console.log(false);
-      return false;
-  }
-  // loop to match each values on each index are the same
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      // if not matching print false
-      console.log("result:", false);
-      return false
-    }
-  }
-  // if they match, print true
-  console.log("result:", true);
-  return true
-};
-
-const assertArraysEqual = function (array1, array2) {
-  // use if statement with eqArray function
-  if (eqArrays(array1, array2)) {
-    console.log("These two arrays are the same.")
-  } else {
-    console.log("These two arrays are not the same.");
-  }
-}
-
-
 const middle = function(array) {
   let middleArray = [];
     if (array.length % 2 === 0) {
@@ -39,15 +10,5 @@ const middle = function(array) {
     }
     return middleArray;
   }
-console.log(middle([1,2,"hello",4,5,6]));
 
-
-assertArraysEqual(middle([2,4,5]), [4]); //true
-// TEST CODE
-// ...
-
-assertArraysEqual(middle([1]),[1]); // true
-assertArraysEqual(middle([1,2]),[1]); // false
-assertArraysEqual(middle([1,2,3]),[2]); // ture
-assertArraysEqual(middle([1,2,3,4]),[2,3]); // true
-assertArraysEqual(middle([1,2,3,4,5]),[3,4]); // false
+module.exports = middle;
---
+++ test/middleTest.js
@@ -0,0 +1,8 @@
+const middle = require('../middle');
+const assertArraysEqual = require('../assertArraysEqual');
+
+assertArraysEqual(middle([1]),[1]); // true
+assertArraysEqual(middle([1,2]),[1]); // false
+assertArraysEqual(middle([1,2,3]),[2]); // ture
+assertArraysEqual(middle([1,2,3,4]),[2,3]); // true
+assertArraysEqual(middle([1,2,3,4,5]),[3,4]); // false
