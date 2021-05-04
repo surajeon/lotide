fddb62e0d6e6dbac5beda8252cc5b886563a3208
Sura Jeon <surajeon@gmail.com>
2021-05-04 00:35:24 +0000

    Mocha/Chai applied: assertArraysEqualTest.js

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,16 @@
+const assert = require('chai').assert;
+
 const assertArraysEqual = require('../assertArraysEqual');
 
+describe('#head',() => {
+  it("return two arrays are the same for [1,2,3] and [1,2,3]",() => {
+    assert.strictEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]), "These two arrays are the same.");
+  });
+
+});
 
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); 
+
+
+
+// assertArraysEqual([1, 2, 3], [1, 2, 3]); 
